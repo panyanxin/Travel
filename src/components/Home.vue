8a13dc724446e8bc71d58ebdab31c257e508d441
panyanxin <1205307815@qq.com>
2019-04-25 19:00:46 +0800

    project init

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class='home'>
+        this is home
+        <router-link to='/list'>列表页</router-link>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+.home{
+    font-size: 50px
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class='home'>
+        this is list
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+.home{
+    font-size: 50px
+}
+</style>
