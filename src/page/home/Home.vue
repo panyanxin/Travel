898c889ef337022ff34264357c682e90363c9f71
panyanxin <1205307815@qq.com>
2019-04-26 09:51:06 +0800

    add icons

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -2,17 +2,20 @@
     <div class='home'>
       <home-header></home-header>
       <home-swiper></home-swiper>
+      <home-icons></home-icons>
         <!-- <router-link to='/list'>列表页</router-link> -->
     </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
     name:'home',
     components: {
         HomeHeader,
-        HomeSwiper
+        HomeSwiper,
+        HomeIcons
     }
 }
 </script>
