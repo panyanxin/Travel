09e5f147238eabbebcf891a225d3299ef013b323
panyanxin <1205307815@qq.com>
2019-04-26 18:09:22 +0800

    search input

--- src/page/city/City.vue
+++ src/page/city/City.vue
@@ -1,13 +1,16 @@
 <template>
     <div class=''>
         <city-header></city-header>    
+        <city-search></city-search>    
     </div>
 </template>
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
