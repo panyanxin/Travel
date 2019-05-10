4d366aa81034413848f29775875ae629de926f01
panyanxin <1205307815@qq.com>
2019-05-10 12:42:20 +0800

    search finish

--- src/page/city/components/Search.vue
+++ src/page/city/components/Search.vue
@@ -1,11 +1,65 @@
 <template>
-    <div class='search'>
-        <input class='search-input' type="text" placeholder="输入城市名或拼音">
-    </div>
+   <div>
+        <div class='search'>
+            <input v-model="Ival" class='search-input' type="text" placeholder="输入城市名或拼音">
+        </div>
+        <div class="search-content" ref='search' v-show='Ival'>
+            <ul>
+                <li 
+                class='search-item border-bottom' 
+                v-for="item of list" 
+                :key='item.id'>{{item.name}}</li>
+                 <li class="search-item border-bottom" v-show="hasNoData">
+                没有找到匹配数据
+                </li>
+            </ul>
+        </div>
+   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name:'CitySearch'
+    name:'CitySearch',
+    props:{
+        cities:Object
+    },
+    data() {
+        return {
+            Ival:'',
+            list:[],
+            timer:null
+        }
+    },
+    computed: {
+      hasNoData(){
+          return !this.list.length
+      }  
+    },
+    watch: {
+        Ival(){
+            if(this.timer){
+                clearTimeout(this.timer)
+            }
+            if(!this.Ival){
+                this.list=[]
+                return
+            }
+            this.timer=setTimeout(()=>{
+                const result =[]
+                for(let i in this.cities){
+                    this.cities[i].forEach((value) => {
+                        if(value.spell.indexOf(this.Ival)>-1||value.name.indexOf(this.Ival)>-1){
+                            result.push(value)
+                        }
+                    });
+                }
+                this.list =result
+            },100)
+        }
+    },
+    mounted() {
+        this.scroll=new Bscroll(this.$refs.search)
+    },
 }
 </script>
 <style lang='stylus' scoped>
@@ -22,5 +76,19 @@ export default {
             line-height .62rem
             text-align center
             border-radius .06rem
+            color #666
+    .search-content
+        overflow hidden
+        z-index 1
+        position absolute
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+        background #eee
+        .search-item
+            line-height .62rem
+            padding-left .2rem
+            background #fff
             color #666
 </style>
