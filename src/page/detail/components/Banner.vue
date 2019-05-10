52cbb4134406e70f5420b55ecde6aa819e7afa80
panyanxin <1205307815@qq.com>
2019-05-11 01:54:27 +0800

    banner finish

--- src/page/detail/components/Banner.vue
+++ src/page/detail/components/Banner.vue
@@ -1,18 +1,42 @@
 <template>
-    <div class="banner">
-      <img class='banner-img' src="http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg" alt="">
-      <div class="banner-info">
-          <div class="banner-tit">南山滑雪场</div>
-          <div class="banner-num">
-              <span class='iconfont banner-icon'>&#xe625;</span>
-              39
-          </div>
-      </div>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class='banner-img' src="http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-tit">南山滑雪场</div>
+                <div class="banner-num">
+                    <span class='iconfont banner-icon'>&#xe625;</span>
+                    39
+                </div>
+            </div>
+        </div>
+        <common-gallary 
+        :imgs='imgs' 
+        v-show='showGallary'
+        @close='handlegallaryClose'
+        ></common-gallary>
     </div>
 </template>
 <script>
+import CommonGallary from 'common/gallary/Gallary'
 export default {
-    
+    data(){
+        return{
+            showGallary:false,
+            imgs:['https://himg4.qunarzz.com/imgs/201905/10/C.3ukLtwxSQIl59B_2x480.jpg','https://himg1.qunarzz.com/imgs/201812/12/C._M0DCii9aSIdbO0Ei480.jpg']
+        }
+    },
+    components: {
+        CommonGallary
+    },
+    methods: {
+        handleBannerClick(){
+           this.showGallary =true
+        },
+        handlegallaryClose(){
+           this.showGallary =false
+        }
+    }
 }
 </script>
 <style lang="stylus" scoped>
