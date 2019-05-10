27fc03450c3dae2127a9b04451acb81d3d0e2dff
panyanxin <1205307815@qq.com>
2019-05-10 16:09:35 +0800

    finish page

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -14,6 +14,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default {
     name:'home',
     components: {
@@ -25,23 +26,25 @@ export default {
     },
     data(){
         return{
-            city:'',
+            lastCity:'',
             swiperList:[],
             iconList:[],
             recommendList:[],
             weekendList:[]
         }
     },
+    computed: {
+      ...mapState(['city'])  
+    },
     methods:{
         getHomeInfo(){
-            axios.get('/api/index.json')
+            axios.get('/api/index.json?city=' + this.city)
             .then(this.getHomeInfoSucc)
         },
         getHomeInfoSucc(res){
             res=res.data
             if(res.ret&&res.data){
                 const data =res.data
-                this.city =data.city
                 this.swiperList =data.swiperList
                 this.iconList =data.iconList
                 this.recommendList =data.recommendList
@@ -51,7 +54,13 @@ export default {
     },
     mounted () {
         this.getHomeInfo()
-    }
+    },
+    activated() {
+        if(this.lastCity!==this.city){
+            this.getHomeInfo()
+            this.lastCity=this.city
+        }
+    },
 }
 </script>
 <style lang="">
